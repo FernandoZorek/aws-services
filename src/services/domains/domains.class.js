791d2218ca7a247bc37c86b111ed63c44e2fb97d
Fernando.Zorek <fernando.zorek@icolabora.com>
2024-04-30 19:11:27 -0300

    Configured domains service aws

--- src/services/domains/domains.class.js
+++ src/services/domains/domains.class.js
@@ -1,11 +1,29 @@
 /* eslint-disable no-unused-vars */
+const AWS = require('aws-sdk');
+const { BadRequest, Conflict } = require('@feathersjs/errors');
+
 exports.Domains = class Domains {
-  constructor (options) {
+  constructor (options, app) {
     this.options = options || {};
+    this.app = app;
+
+    const aws = app.get('aws');
+    this.awsdomain = aws.domain;
+    this.ip = aws.ip;
+    this.hostedZoneId = aws.zone;
+    this.route53 = new AWS.Route53({
+      accessKeyId: aws.accessKeyId,
+      secretAccessKey: aws.secretAccessKey,
+      region: aws.region
+    });
   }
 
   async find (params) {
-    return [];
+    const allZones = await this.route53.listHostedZones().promise();
+    const filteredZones = allZones.HostedZones.filter(zone => zone.Name === this.awsdomain + '.');
+    const zoneId = filteredZones[0].Id;
+    const zoneRecords = await this.route53.listResourceRecordSets({ HostedZoneId: zoneId, MaxItems: '1000' }).promise();
+    return zoneRecords.ResourceRecordSets.filter(ref => ref.Type === 'A');
   }
 
   async get (id, params) {
@@ -15,11 +33,31 @@ exports.Domains = class Domains {
   }
 
   async create (data, params) {
-    if (Array.isArray(data)) {
-      return Promise.all(data.map(current => this.create(current, params)));
+    if (!data.subdomain) throw new BadRequest('subdomain is required');
+    try {
+      const subdomain = await this.route53.changeResourceRecordSets({
+        HostedZoneId: this.hostedZoneId,
+        ChangeBatch: {
+          Changes: [{
+            Action: 'CREATE',
+            ResourceRecordSet: {
+              Name: `${data.subdomain}.${this.awsdomain}`,
+              Type: 'A',
+              TTL: 300,
+              ResourceRecords:
+              [
+                {
+                  Value: this.ip
+                }
+              ]
+            }
+          }]
+        }
+      }).promise();
+      return subdomain;
+    } catch (e) {
+      throw new Conflict(e.message);
     }
-
-    return data;
   }
 
   async update (id, data, params) {
@@ -31,6 +69,30 @@ exports.Domains = class Domains {
   }
 
   async remove (id, params) {
-    return { id };
+    if (!id) throw new BadRequest('id/subdomain is required');
+    try {
+      const subdomain = await this.route53.changeResourceRecordSets({
+        HostedZoneId: this.hostedZoneId,
+        ChangeBatch: {
+          Changes: [{
+            Action: 'DELETE',
+            ResourceRecordSet: {
+              Name: `${id}.${this.awsdomain}`,
+              Type: 'A',
+              TTL: 300,
+              ResourceRecords:
+              [
+                {
+                  Value: this.ip
+                }
+              ]
+            }
+          }]
+        }
+      }).promise();
+      return subdomain;
+    } catch (e) {
+      throw new Error(e.message);
+    }
   }
 };
